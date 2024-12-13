85b4ff89e0b307f083704217550b92d54cb8d3c6
hanyeong0815 <yoehadu815@gmail.com>
2024-12-13 15:05:39 +0900

    first commit

---
+++ src/main/java/com/side/moneymoriapi/MoneyMoriApiApplication.java
@@ -0,0 +1,13 @@
+package com.side.moneymoriapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMoriApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyMoriApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/side/moneymoriapi/MoneyMoriApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.moneymoriapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMoriApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
